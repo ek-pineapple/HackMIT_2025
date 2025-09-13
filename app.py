bcaf24ead7d6de11eb7ef5882b99efab7368865e
Sabia Irfan <sabiairfan111@gmail.com>
2025-09-13 16:24:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,9 @@ app.config["SECRET_KEY"] = "oauh"
 
 model = WhisperModel("tiny")
 
-
 @app.route("/")
 def home():
-    return render_template("speech_to_text.html")
+    return render_template("text_to_text.html")
 
 @app.route("/speech_to_text", methods=["POST"])
 def speech_to_text():
@@ -25,6 +24,24 @@ def speech_to_text():
 
     return jsonify({"transcript": transcript})
 
+@app.route('/submit_reflection', methods=['POST'])
+def submit_reflection():
+    submitted = False
+    work_done = request.form.get('work', '')
+    hours_worked = request.form.get('hours', '0')
+    help_self = request.form.get('help', '')
+    future_impact = request.form.get('impact', '')
+
+    submitted = True
+
+    return render_template(
+        'text_to_text.html',
+        submitted=submitted,
+        work_done=work_done,
+        hours_worked=hours_worked,
+        help_self=help_self,
+        future_impact=future_impact
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
