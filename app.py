4a5b20b3d296716dbf22b883e47d74a1b6659d9b
Sabia Irfan <sabiairfan111@gmail.com>
2025-09-13 17:36:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = WhisperModel("tiny")
 
 @app.route("/")
 def home():
-    return render_template("text_to_text.html")
+    return render_template("speech_to_text.html")
 
 @app.route("/speech_to_text", methods=["POST"])
 def speech_to_text():
@@ -23,25 +23,6 @@ def speech_to_text():
     transcript = " ".join([seg.text for seg in segments])
 
     return jsonify({"transcript": transcript})
-
-@app.route('/submit_reflection', methods=['POST'])
-def submit_reflection():
-    submitted = False
-    work_done = request.form.get('work', '')
-    hours_worked = request.form.get('hours', '0')
-    help_self = request.form.get('help', '')
-    future_impact = request.form.get('impact', '')
-
-    submitted = True
-
-    return render_template(
-        'text_to_text.html',
-        submitted=submitted,
-        work_done=work_done,
-        hours_worked=hours_worked,
-        help_self=help_self,
-        future_impact=future_impact
-    )
 
 if __name__ == '__main__':
     app.run(debug=True)
